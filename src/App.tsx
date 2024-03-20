6533e64ebee55e814799047a429c8b4112e413b5
Bill <96billc@gmail.com>
2024-03-20 12:14:50 +0000

    mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [recipient, setRecipient] = useState("");
+  const [userAddress, setUserAddress] = useState(
+    localStorage.getItem("userAddress") ?? ""
+  );
+
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const derivedEmail = () => `${recipient.replace("@", "_at_")}_${userAddress}`;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h2>Duck.com Email Builder</h2>
+      <p>Recipient</p>
+      <input
+        type="text"
+        placeholder=""
+        value={recipient}
+        onChange={(e) => setRecipient(e.target.value)}
+      />
+      <br></br>
+      <p>Your email address</p>
+      <input
+        type="text"
+        value={userAddress}
+        onChange={(e) => {
+          setUserAddress(e.target.value);
+          localStorage.setItem("userAddress", e.target.value);
+        }}
+      />
+      {userAddress.endsWith("@duck.com") && isValidEmail(recipient) && (
+        <>
+          <h3>{derivedEmail()}</h3>
+          <button
+            onClick={() => {
+              navigator.clipboard.writeText(derivedEmail());
+            }}
+          >
+            Copy
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
